82010db2408e8e9327dca40cacfa7a69d1a64b20
DevAmkam <amadu@fixsolution.sl>
2021-03-03 22:49:40 +0100

    Finished styling homepage

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import MenuItem from '../menu-items/MenuItem'
+import './directory.styles.scss'
+
+export default class Directory extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      sections: [
+        {title: 'hats', imageUrl: 'https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80', id: 1},
+        {title: 'jackets', imageUrl: 'https://images.unsplash.com/photo-1604644401890-0bd678c83788?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80', id: 2},
+        {title: 'sneakers', imageUrl: 'https://images.unsplash.com/photo-1597248881519-db089d3744a5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80', id: 3},
+        {title: "women's", size: 'large', imageUrl: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80', id: 4},
+        {title: "men's", size: 'large', imageUrl: 'https://images.unsplash.com/photo-1473492201326-7c01dd2e596b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=751&q=80', id: 5},
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {
+          this.state.sections.map(({title, imageUrl, id, size}) => (
+            <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
+          ))
+        }
+      </div>
+    )
+  }
+}
