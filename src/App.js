13695fbfb5edbf06d40bfdcf3c71ef329a8feb41
DevAmkam <amadu@fixsolution.sl>
2021-03-04 09:17:47 +0100

    Configured categories routes with React router dom package.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { Route } from 'react-router-dom'
+
 import Homepage from './pages/homepage/Homepage'
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={Homepage} />
     </div>
   );
 }
--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -8,11 +8,11 @@ export default class Directory extends Component {
 
     this.state = {
       sections: [
-        {title: 'hats', imageUrl: 'https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80', id: 1},
-        {title: 'jackets', imageUrl: 'https://images.unsplash.com/photo-1604644401890-0bd678c83788?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80', id: 2},
-        {title: 'sneakers', imageUrl: 'https://images.unsplash.com/photo-1597248881519-db089d3744a5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80', id: 3},
-        {title: "women's", size: 'large', imageUrl: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80', id: 4},
-        {title: "men's", size: 'large', imageUrl: 'https://images.unsplash.com/photo-1473492201326-7c01dd2e596b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=751&q=80', id: 5},
+        {title: 'hats', imageUrl: 'https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80', id: 1, linkUrl: 'hats'},
+        {title: 'jackets', imageUrl: 'https://images.unsplash.com/photo-1604644401890-0bd678c83788?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80', id: 2, linkUrl: 'jackets'},
+        {title: 'sneakers', imageUrl: 'https://images.unsplash.com/photo-1597248881519-db089d3744a5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80', id: 3, linkUrl: 'sneakers'},
+        {title: "women's", size: 'large', imageUrl: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80', id: 4, linkUrl: 'women'},
+        {title: "men's", size: 'large', imageUrl: 'https://images.unsplash.com/photo-1473492201326-7c01dd2e596b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=751&q=80', id: 5, linkUrl: 'men'},
       ]
     }
   }
@@ -20,8 +20,8 @@ export default class Directory extends Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({title, imageUrl, id, size}) => (
-            <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({id, ...otherSectionProps}) => (
+            <MenuItem  key={id} {...otherSectionProps} />
           ))
         }
       </div>
